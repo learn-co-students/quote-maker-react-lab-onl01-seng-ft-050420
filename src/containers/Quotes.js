63e262f79c99812362f2ce0b25cd55b2d651ccf2
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-09-12 20:32:22 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import QuoteCard from '../components/QuoteCard';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import QuoteCard from "../components/QuoteCard";
+import { removeQuote, upvoteQuote, downvoteQuote } from "../actions/quotes";
 
 class Quotes extends Component {
-
   render() {
     return (
       <div>
         <hr />
-        <div className="row justify-content-center">
+        <div className='row justify-content-center'>
           <h2>Quotes</h2>
         </div>
         <hr />
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4'>
+              {this.props.quotes.map((quote) => (
+                <QuoteCard
+                  key={quote.id}
+                  upvoteQuote={this.props.upvoteQuote}
+                  downvoteQuote={this.props.downvoteQuote}
+                  removeQuote={this.props.removeQuote}
+                  quote={quote}
+                />
+              ))}
             </div>
           </div>
         </div>
@@ -28,5 +32,14 @@ class Quotes extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = (state) => {
+  return {
+    quotes: state.quotes,
+  };
+};
+
+export default connect(mapStateToProps, {
+  removeQuote,
+  upvoteQuote,
+  downvoteQuote,
+})(Quotes);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
-import { combineReducers } from 'redux';
-import quotes from './quotes';
+import { combineReducers } from "redux";
+import quotes from "./quotes";
+import uuid from "uuid";
+
+const id = uuid();
+console.log(id);
 
 export default combineReducers({
-  quotes
+  quotes,
 });
