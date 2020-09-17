e92b446d9d11637fefcd818ed06aa7861ecc7a69
anette87 <anettem.rivera@gmail.com>
2020-09-17 14:18:39 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,39 @@
+
+const findWithIndex = (arr, predicate) => {
+  const index = arr.findIndex(predicate);
+  return {
+    item: index !== -1 ? arr[index] : null,
+    index,
+  };
+};
+
 export default (state = [], action) => {
-  return state;
-}
+  let quote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+    case 'REMOVE_QUOTE':
+      return state.filter((quote) => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      quote = findWithIndex(state, (item) => item.id === action.quoteId);
+      quote.item.votes++;
+      return [
+        ...state.slice(0, quote.index),
+        quote.item,
+        ...state.slice(quote.index + 1),
+      ];
+    case 'DOWNVOTE_QUOTE':
+      quote = findWithIndex(state, (item) => item.id === action.quoteId);
+      if (quote.item.votes !== 0) {
+        quote.item.votes--;
+      }
+      return [
+        ...state.slice(0, quote.index),
+        quote.item,
+        ...state.slice(quote.index + 1),
+      ];
+    default:
+      return state;
+  }
+};
