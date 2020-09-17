c5bb3f77161fdb50c50f348790a7d4f2d46d0bb9
leobr <leobrionesherrera@gmail.com>
2020-09-17 15:22:30 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,4 @@
-// TODO: Create action creators as defined in tests
+export const addQuote = (quote) => ({type: "ADD_QUOTE", quote: quote});
+export const removeQuote = (id) => ({type: "REMOVE_QUOTE", quoteId: id});
+export const upvoteQuote = (id) => ({type: "UPVOTE_QUOTE", quoteId: id});
+export const downvoteQuote = (id) => ({type: "DOWNVOTE_QUOTE", quoteId: id});
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,22 @@
-export default (state = [], action) => {
-  return state;
+function quotes(state=[], action) {
+  switch(action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter((el) => el.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      return state.map((el) => {
+         if(el.id === action.quoteId) el.votes++;
+         return el;
+        });
+    case "DOWNVOTE_QUOTE":
+      return state.map((el) => {
+        if(el.id === action.quoteId && el.votes > 0) el.votes--;
+        return el;
+      });
+    default:
+      return state;
+  }
 }
+
+export default quotes;
