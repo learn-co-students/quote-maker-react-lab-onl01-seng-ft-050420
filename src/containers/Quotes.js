1f47fa4652f75181ba638992d1e035cb57ac03d9
claudiahalip <claudia4ro@yahoo.com>
2020-09-17 21:23:13 -0500

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { upvoteQuote, downvoteQuote, removeQuote } from '../actions/quotes'
 
 class Quotes extends Component {
+
+  quotesForDisplay = ()=>{
+    //debugger
+    return this.props.quotes.map(quote => <QuoteCard 
+      upvoteQuote={this.props.upvoteQuote}
+      downvoteQuote={this.props.downvoteQuote}
+      removeQuote = {this.props.removeQuote}
+      quote={quote}
+      
+    />)
+  }
 
   render() {
     return (
@@ -19,7 +31,11 @@ class Quotes extends Component {
                 TODO:
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
+                
                */}
+              
+
+              {this.quotesForDisplay()}
             </div>
           </div>
         </div>
@@ -28,5 +44,9 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  return {quotes: state.quotes}
+}
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, {upvoteQuote, downvoteQuote, removeQuote})(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,27 @@
+
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      return state.map( quote => {
+          if(quote.id === action.quoteId) quote.votes++;
+          return quote
+      })
+      case 'DOWNVOTE_QUOTE':
+        let index = state.findIndex(quote => quote.id ===action.quoteId)
+        if(state[index].votes > 0){
+          return [
+            ...state.slice(0,index),
+            {...state[index], votes: (state[index].votes -= 1)},
+            ...state.slice(index+1),
+          ]
+        }
+   default:
+    return state;
+  }
 }
+
